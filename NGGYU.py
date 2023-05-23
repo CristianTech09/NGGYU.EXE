ad8d41b273d765d041d4362c20031278d11a4f3b
Cristian <106407866+CristianTech09@users.noreply.github.com>
2023-05-23 19:17:27 +0200

    Add files via upload

--- NGGYU.py
+++ NGGYU.py
@@ -1,5 +1,11 @@
 import os
 
-print ('Hello World!')
+os.system('title Hello World!')
+
+os.system('mode con: cols=70 lines=37')
 
 os.system('explorer.exe "https://www.youtube.com/watch?v=dQw4w9WgXcQ"')
+
+os.system('color 0a')
+
+os.system('curl ascii.live/rick')
